83a11e075c48e388811e2e03e62d3e226aaf0397
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-05-07 18:51:44 +0200

    Weater app using react app

---
+++ src/components/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import Form from "./Form";
+import Result from "./Result";
+import "./App.css";
+//Klucz do API
+const APIkey = "882cb5500894ec7e4604a5304d6d6507";
+
+class App extends Component {
+  state = {
+    value: "",
+    date: "",
+    city: "",
+    sunrise: "",
+    sunset: "",
+    temp: "",
+    pressure: "",
+    wind: "",
+    err: false,
+  };
+
+  handleInputChage = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  handleCitySubmit = (e) => {
+    e.preventDefault();
+
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIkey}&units=metric`;
+
+    fetch(API)
+      .then((response) => {
+        if (response.ok) {
+          return response;
+        }
+        throw Error("Nie udało się");
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        const time = new Date().toLocaleString();
+        this.setState((state) => ({
+          err: false,
+          date: time,
+          sunrise: data.sys.sunrise,
+          sunset: data.sys.sunset,
+          temp: data.main.temp,
+          pressure: data.main.pressure,
+          wind: data.wind.speed,
+          city: state.value,
+        }));
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState((prevState) => {
+          return {
+            err: true,
+            city: prevState.value,
+          };
+        });
+      });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Form
+          value={this.state.value}
+          change={this.handleInputChage}
+          submit={this.handleCitySubmit}
+        />
+        <Result weather={this.state} />
+      </div>
+    );
+  }
+}
+
+export default App;
