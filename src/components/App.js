cb821d31d94ef393c57324c8daf11bdd43d885dc
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-05-08 15:45:48 +0200

    weather app without button in react-app

--- src/components/App.js
+++ src/components/App.js
@@ -24,50 +24,87 @@ class App extends Component {
     });
   };
 
-  handleCitySubmit = (e) => {
-    e.preventDefault();
+  // handleCitySubmit = (e) => {
+  //   e.preventDefault();
 
-    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIkey}&units=metric`;
+  //   const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIkey}&units=metric`;
 
-    fetch(API)
-      .then((response) => {
-        if (response.ok) {
-          return response;
-        }
-        throw Error("Nie udało się");
-      })
-      .then((response) => response.json())
-      .then((data) => {
-        const time = new Date().toLocaleString();
-        this.setState((state) => ({
-          err: false,
-          date: time,
-          sunrise: data.sys.sunrise,
-          sunset: data.sys.sunset,
-          temp: data.main.temp,
-          pressure: data.main.pressure,
-          wind: data.wind.speed,
-          city: state.value,
-        }));
-      })
-      .catch((err) => {
-        console.log(err);
-        this.setState((prevState) => {
-          return {
-            err: true,
-            city: prevState.value,
-          };
+  //   fetch(API)
+  //     .then((response) => {
+  //       if (response.ok) {
+  //         return response;
+  //       }
+  //       throw Error("Nie udało się");
+  //     })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const time = new Date().toLocaleString();
+  //       this.setState((state) => ({
+  //         err: false,
+  //         date: time,
+  //         sunrise: data.sys.sunrise,
+  //         sunset: data.sys.sunset,
+  //         temp: data.main.temp,
+  //         pressure: data.main.pressure,
+  //         wind: data.wind.speed,
+  //         city: state.value,
+  //       }));
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       this.setState((prevState) => {
+  //         return {
+  //           err: true,
+  //           city: prevState.value,
+  //         };
+  //       });
+  //     });
+  // };
+
+  componentDidUpdate(prevProps, prevState) {
+    // console.log("poprzednia wartość " + prevState.value);
+    // console.log("aktualna wartość " + this.state.value);
+
+    if (this.state.value.length === 0) return;
+    if (prevState.value !== this.state.value) {
+      const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIkey}&units=metric`;
+
+      fetch(API)
+        .then((response) => {
+          if (response.ok) {
+            return response;
+          }
+          throw Error("Nie udało się");
+        })
+        .then((response) => response.json())
+        .then((data) => {
+          const time = new Date().toLocaleString();
+          this.setState((state) => ({
+            err: false,
+            date: time,
+            sunrise: data.sys.sunrise,
+            sunset: data.sys.sunset,
+            temp: data.main.temp,
+            pressure: data.main.pressure,
+            wind: data.wind.speed,
+            city: state.value,
+          }));
+        })
+        .catch((err) => {
+          console.log(err);
+          this.setState((prevState) => {
+            return {
+              err: true,
+              city: prevState.value,
+            };
+          });
         });
-      });
-  };
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Form
-          value={this.state.value}
-          change={this.handleInputChage}
-          submit={this.handleCitySubmit}
-        />
+        <Form value={this.state.value} change={this.handleInputChage} />
         <Result weather={this.state} />
       </div>
     );
